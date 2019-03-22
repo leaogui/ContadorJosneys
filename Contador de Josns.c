81de0ab8316cb671212b2b2da526aefe10c5f20f
leaogui <guiladriero@hotmail.com>
2019-03-22 10:46:36 -0300

    Continuando programa

--- Contador de Josns.c
+++ Contador de Josns.c
@@ -3,30 +3,55 @@
 #include<locale.h>
 
 
-int processo(int josney){
+int processo(int josney, char aux){
 	
-	char stat;
 	
-	puts("Digite enter para adicionar 1 no contador, um número de 0 até 9 para adicionar/diminuir, N para adicionar um número maior, S para salvar e X para salvar e sair.\n");	
+	if(aux = 'N' || aux = 'n'){
+		
+		printf("Digite o número para ser adicionado: ");
+		
+		scanf("%d",josney);
+		
+		josney += aux;
+		
+		
+	}else{
+		
+			josney++;
+			
+		}
+	}
 	
-	printf("Escolha: ");
-	
-	scanf("%c", &stat);
 	
 	return josney;
 }
 
 void controlador(int josney){
 	
-	
+	char aux;
 	
 	puts("Bem vindo ao contador de Josneys\n");
 	
-	while (aux != 999){
+	while (2>1){
 	
 	printf("O número de Josneys até agora é: %d\n", josney);
 	
-	processo(josney);
+	puts("Digite enter ou um dígito diferente dos próximos para adicionar 1 no contador, N para adicionar um número maior, S para salvar e X para salvar e sair.\n");	
+	
+	printf("Escolha: ");
+	
+	scanf("%c", &aux);
+	
+	if(aux != 'x' || aux != 'X'){
+		
+		josney = processo(josney, aux);
+		
+	}else{
+		
+		system("cls");
+		
+		break;
+	}
 	
 	system("cls");
 	
