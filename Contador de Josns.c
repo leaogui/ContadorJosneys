95464b489c1022487c0685f25deb01e3f1e25f7c
gghosth <42755106+gghosth@users.noreply.github.com>
2019-03-23 12:47:43 -0300

    Versão base do programa, funcionando

--- Contador de Josns.c
+++ Contador de Josns.c
@@ -2,17 +2,22 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int processo(int josney, char aux, FILE* arq)
+int bk;
+
+int processo(int josney, char aux)
 {
+    FILE* arqbk;
+
+    arqbk = fopen("Backup.xml", "w+");
 
     if (aux == 'N' || aux == 'n') {
-    	
-    	int temp;
+
+        int temp;
 
         printf("\nDigite o número para ser adicionado: ");
-	
-		fflush(stdin);
-	
+
+        fflush(stdin);
+
         scanf("%d", &temp);
 
         josney += temp;
@@ -21,8 +26,12 @@ int processo(int josney, char aux, FILE* arq)
 
         josney++;
     }
-    
-     system("cls");
+
+    system("cls");
+
+    fprintf(arqbk, "%d", josney);
+
+    fclose(arqbk);
 
     return (josney);
 }
@@ -38,82 +47,158 @@ void controlador(int josney, FILE* arq)
 
         printf("\nO número de Josneys até agora é: %d\n\n", josney);
 
-        puts("Digite enter ou um dígito diferente dos próximos para adicionar 1 no contador, N para adicionar um número maior, S para salvar e X para salvar e sair.\n");
+        puts("Digite enter ou um dígito diferente dos próximos para adicionar 1 no contador, N para adicionar um número maior, S para salvar, I para informações adicionais do programa, B para fazer um backup e X para salvar e sair.\n");
 
         printf("Escolha: ");
 
-		fflush(stdin);
+        fflush(stdin);
 
         scanf("%c", &aux);
 
-        if (aux == 'x' || aux == 'X') {
+        if (aux == 'i' || aux == 'I') {
 
-			arq = fopen("Contador.xml", "w+");
-				
-			fprintf(arq,"%d", josney);
-			
-			system("cls");
+            system("cls");
 
-            break;
+            puts("\n\n Este programa conta o número de Josneys que ocorreram até o momento.\n Sempre que um Josney é contado o programa faz um backup, assim como quando ele é terminado pelo X. \n Programa para descontrair feito por Guilherme Leão. @guivsthewold\n\n");
+
+            system("pause");
+
+            system("cls");
         }
         else {
-        	
-			if(aux == 's' || aux == 'S'){		
-				
-				
-				arq = fopen("Contador.xml", "w+");
-				
-				fprintf(arq,"%d", josney);
-				
-				 system("cls");
-				
-				puts("O número foi salvo no txt!\n");
-				
-				
-				}
-				
-				else{
-				
 
-            josney = processo(josney, aux, arq);
-            
-            
-			}
+            if (aux == 'b' || aux == 'B') {
+
+                system("cls");
+
+                FILE* bkarq;
+
+                int temp;
+
+                char auxbk;
+
+                bkarq = fopen("Backup.xml", "r+");
+
+                fflush(stdin);
+
+                fscanf(bkarq, "%d", &temp);
+
+                printf("O número de Josneys no backup é de: %d, deseja mesmo fazer o backup?\n", temp);
+
+                printf("S para sim, N para não: ");
+
+                fflush(stdin);
+
+                scanf("%c", &auxbk);
+
+                if (auxbk == 's' || auxbk == 'S') {
+
+                    josney = temp;
+
+                    system("cls");
+
+                    puts("\nO backup foi feito.\n\n");
+
+                    fclose(bkarq);
+                }
+                else {
+
+                    system("cls");
+
+                    puts("\nO backup não foi feito.\n\n");
+
+                    fclose(bkarq);
+                }
+            }
+            else {
+                if (aux == 'x' || aux == 'X') {
+
+                    FILE* arqtxt;
+
+                    arq = fopen("Contador.xml", "w+");
+
+                    arqtxt = fopen("Josneys.txt", "w+");
+
+                    fprintf(arq, "%d", josney);
+
+                    fprintf(arqtxt, "O número de Josneys até agora é: %d\n", josney);
+
+                    fclose(arq);
+
+                    fclose(arqtxt);
+
+                    system("cls");
+
+                    bk = josney;
+
+                    break;
+                }
+                else {
+
+                    if (aux == 's' || aux == 'S') {
+
+                        FILE* arqtxt;
+
+                        arq = fopen("Contador.xml", "w+");
+
+                        arqtxt = fopen("Josneys.txt", "w+");
+
+                        fprintf(arq, "%d", josney);
+
+                        fprintf(arqtxt, "O número de Josneys até agora é: %d\n", josney);
+
+                        system("cls");
+
+                        puts("O número foi salvo no txt!\n");
+
+                        fclose(arq);
+
+                        fclose(arqtxt);
+
+                        bk = josney;
+                    }
+
+                    else {
+
+                        josney = processo(josney, aux);
+                    }
+                }
+            }
         }
-
-       
     }
 }
-
-
-
 
 int main(void)
 {
 
-    FILE* arq;
+    FILE *arq, *arqbk;
 
     setlocale(LC_ALL, "portuguese");
 
     int josney;
 
-    arq = fopen("Contador.xml", "r+");
-    
+    arq = fopen("Contador.xml", "r");
 
-	fflush(stdin);
+    fflush(stdin);
 
     fscanf(arq, "%d", &josney);
 
+    fclose(arq);
+
     controlador(josney, arq);
 
-    puts("\n\n                    Obrigado por utilizar o programa!!!\n\n                    Os Josneys foram salvos no arquivo Josneys.txt.\n\n");
+    arqbk = fopen("backup.xml", "w+");
+
+    fprintf(arqbk, "%d", bk);
+
+    fclose(arqbk);
+
+    puts("\n\n                    Obrigado por utilizar o programa!!!\n\n                    Os Josneys foram salvos no arquivo Josneys.txt e um backup foi feito.\n\n");
 
     fclose(arq);
 
     system("pause");
-    
+
     return 0;
 }
-
-
 
